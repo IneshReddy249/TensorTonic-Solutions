521400445cc9a33db15d6597da2ef854b7d6209c
Inesh Reddy Chappidi <ineshreddy249@gmail.com>
2026-07-24 15:51:23 -0400

    Solve pytorch-tensor-reshape

---
+++ study-plans/pytorch-basics/pytorch-tensor-reshape/pytorch-tensor-reshape.py
@@ -0,0 +1,16 @@
+import torch
+
+def reshape_tensor(x, op):
+    """
+    Returns: list
+    """
+    x = torch.tensor(x, dtype= float)
+    if op == "flatten":
+        return (x.flatten()).tolist()
+        
+    elif op == "squeeze":
+        return (x.squeeze()).tolist()
+
+    elif op == "transpose":
+        return (x.T).tolist()
+    pass
